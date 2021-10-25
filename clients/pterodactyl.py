0a1ae5ae000ff17c8ccdab26f40693e143ec7c89
Ian M <theianm2002@gmail.com>
2021-10-24 21:45:36 -0400

    Perform auth handshake

--- clients/pterodactyl.py
+++ clients/pterodactyl.py
@@ -1,24 +1,53 @@
 import asyncio
+import httpx
 import websockets
+import json
+from urllib.parse import urlparse
+
+import typing
 
 
 class PterodactylClient:
-    websocket = None
+    loop: asyncio.AbstractEventLoop = None
+    httpx_client: httpx.AsyncClient = None
+    panel_url: str = None
+    server_id: str = None
+    websocket: websockets.WebSocketClientProtocol = None
 
-    def __init__(self):
+    def __init__(self, panel_url: str, api_key: str, server_id: str):
+        self.server_id = server_id
+
+        # Normalize panel url to https://example.com
+        panel_url_parsed = urlparse(panel_url)
+        self.panel_url = f'{panel_url_parsed.scheme}://{panel_url_parsed.netloc}'
+
+        self.httpx_client = httpx.AsyncClient(base_url=self.panel_url, headers={'authorization': f'Bearer {api_key}'})
         self.loop = asyncio.get_event_loop()
-        self.loop.run_until_complete(self.establish())
+        self.loop.run_until_complete(self._establish())
 
-    async def establish(self):
-        url = "wss://can1.bennystudios.com:8080/api/servers/8ee05379-42b6-484f-9b8e-56df088648d9/ws"
-        headers = {"origin": "https://panel.bennystudios.com"}
+    async def _establish(self) -> None:
+        _, websocket_url = await self._fetch_websocket_credentials()
+        self.websocket = await websockets.connect(websocket_url, extra_headers={"origin": self.panel_url})
 
-        async with websockets.connect(url, extra_headers=headers) as websocket:
-            await self.consumer_handler(websocket)
+        await self._authorize()
+        await self._consumer_handler(self.websocket)
 
-    async def consumer_handler(self, websocket):
+    async def _authorize(self):
+        auth_token, _ = await self._fetch_websocket_credentials()
+        await self.send('auth', [auth_token])
+
+    async def _consumer_handler(self, websocket: websockets.WebSocketClientProtocol) -> None:
         async for message in websocket:
+            # Among the many todos is resending auth token
             print(f"=> {message}")
 
+    async def _fetch_websocket_credentials(self) -> typing.Tuple[str, str]:
+        resp = await self.httpx_client.get(f'/api/client/servers/{self.server_id}/websocket')
+        resp.raise_for_status()
 
-PterodactylClient()
+        json = resp.json()
+        return json['data']['token'], json['data']['socket']
+
+    async def send(self, event: str, args: list) -> None:
+        object = {"event": event, "args": args}
+        await self.websocket.send(json.dumps(object))
---
+++ main.py
@@ -0,0 +1,8 @@
+import yaml
+
+from clients.pterodactyl import PterodactylClient
+
+with open("config.yml", "r") as stream:
+    config = yaml.safe_load(stream)
+
+    PterodactylClient(config["pterodactyl"]["panel_url"], config["pterodactyl"]["api_key"], config["server"]["id"])

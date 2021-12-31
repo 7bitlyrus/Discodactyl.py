2136ccb5e55f33fdf46c1194a4f73d59e5796d9b
Ian M <theianm2002@gmail.com>
2021-12-30 20:38:15 -0500

    Rewrite to use commands over client

--- discordactyl/__init__.py
+++ discordactyl/__init__.py
@@ -1,49 +1,49 @@
-import asyncio
-import logging
-
 import discord
 import yaml
+import logging
+from discord.ext import commands
 
 from .pterodactyl import PterodactylClient
-
 
 logging.basicConfig(level=logging.INFO)
 
 with open("config.yml", "r") as stream:
     config = yaml.safe_load(stream)
 
-    pterodactyl = PterodactylClient(
-        config["pterodactyl"]["panel_url"],
-        config["pterodactyl"]["api_key"],
-        config["server"]["id"],
-    )
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned,
+    case_insensitive=True,
+    intents=discord.Intents(messages=True),
+)
 
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    asyncio.create_task(pterodactyl.start())  # TODO: CLOSE TASK AND ONLY START ONCE, ETC ,ETC
-    print('[discord] on_ready')
+pterodactyl = PterodactylClient(
+    config["pterodactyl"]["panel_url"],
+    config["pterodactyl"]["api_key"],
+    config["server"]["id"],
+)
 
 
-@client.event
-async def on_message(message):
-    print('[discord] on_message:', message.content)
-    # await pterodactyl.send('send command', [f'say {message.content}'])
+class Bridge(commands.Cog):
+    def __init__(self, bot):
+        self.task = bot.loop.create_task(pterodactyl.start())  # TODO: CLOSE TASK AND ONLY START ONCE, ETC ,ETC
 
-    # await pterodactyl.close()
+    def cog_unload(self):
+        self.task.cancel()
+
+    @commands.Cog.listener()
+    async def on_message(message):
+        print('[discord] on_message:', message.content)
+
+    @pterodactyl.on('stats')
+    async def test(data):
+        print(data)
+
+    @pterodactyl.on('console output')
+    async def output(data):
+        await pterodactyl.send('send command', f'say {data}')
 
 
-@pterodactyl.on('stats')
-async def test(data):
-    print(data)
+bot.add_cog(Bridge(bot))
+bot.load_extension('jishaku')
 
-
-@pterodactyl.on('console output')
-async def output(data):
-    await pterodactyl.send('send command', f'say {data}')
-
-
-client.run(config['discord-token'])
-# asyncio.run(pterodactyl.start())
+bot.run(config['discord-token'])
